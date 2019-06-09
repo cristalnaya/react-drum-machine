7c5c8efed6aabfb1d6e86e56cbeb953abee7f091
Tina <radostinadimitrova@gmail.com>
2019-06-09 03:18:22 +0300

    feat: Create a data for the drum pads

---
+++ src/data.js
@@ -0,0 +1,56 @@
+export const drumPads = [
+  {
+    id: 'boom',
+    text: 'Q',
+    idClip: 'Q',
+    src: './sounds/boom.wav', 
+  },
+  {
+    id: 'clap',
+    text: 'W',
+    idClip: 'W',
+    src: 'sounds/clap.wav', 
+  },
+  {
+    id: 'hihat',
+    text: 'E',
+    idClip: 'E',
+    src: 'sounds/hihat.wav'
+  },
+  {
+    id: 'kick',
+    text: 'A',
+    idClip: 'A',
+    src: 'sounds/kick.wav', 
+  },
+  {
+    id: 'openhat',
+    text: 'S',
+    idClip: 'S',
+    src: 'sounds/openhat.wav',
+  },
+  {
+    id: 'ride',
+    text: 'D',
+    idClip: 'D',
+    src: 'sounds/ride.wav', 
+  },
+  {
+    id: 'snare',
+    text: 'Z',
+    idClip: 'Z',
+    src: 'sounds/snare.wav', 
+  },
+  {
+    id: 'tink',
+    text: 'X',
+    idClip: 'X',
+    src: 'sounds/tink.wav', 
+  },
+  {
+    id: 'tom',
+    text: 'C',
+    idClip: 'C',
+    src: 'sounds/tom.wav', 
+  },
+]
